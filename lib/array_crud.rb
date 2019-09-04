00d45d014baa2461f707480af70526af9656faf4
celebrated-integer-2023 <scailles23@gmail.com>
2019-09-04 09:48:26 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["a","b","c","d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+ create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+ create_an_array.shift
+end
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_elem
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array(create_an_array.first)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array(create_an_array.last)
 end
